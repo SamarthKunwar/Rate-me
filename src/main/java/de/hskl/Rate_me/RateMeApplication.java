458a224d8a662502457f3d6bf3877d62699db770
Samarth Kunwar <SamarthKunwar2002@gmail.com>
2026-06-03 14:21:54 +0200

    initial commit

---
+++ src/main/java/de/hskl/Rate_me/RateMeApplication.java
@@ -0,0 +1,13 @@
+package de.hskl.Rate_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hskl/Rate_me/RateMeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hskl.Rate_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
